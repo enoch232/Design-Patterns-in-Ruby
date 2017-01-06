058ab4847d41a8e581d386765caf0dc8ac953f6f
enoch232 <enoch232@gmail.com>
2017-01-06 16:27:22 -0700

    make an attempt to use metaprogramming to generate flexible api to remove posts by its attr

--- commands_pattern/command_pattern.rb
+++ commands_pattern/command_pattern.rb
@@ -21,9 +21,8 @@ end
 
 class Post
   attr_accessor :id, :title, :author, :body
-  def initialize(title, author, body)
-  	@@id ||= 0
-  	@id = @@id
+  def initialize(id, title, author, body)
+  	@id = id
   	@title = title
   	@author = author
   	@body = body
@@ -36,16 +35,26 @@ class PostList
   end
 
   def add_post(title, author, body)
-  	@posts << Post.new(title, author, body)
+  	@posts << Post.new(@posts.size, title, author, body)
   end
 
   def remove_post(id)
   	@posts.delete_if{ |post| post.id == id }
   end
 
+  def method_missing(method_name, *args)
+  	method_name = method_name.to_s
+  	unless method_name =~ /remove_post_by_/
+      return
+  	end
+    letters = method_name.split('_')
+    delete_by = letters[3]
+    send()
+  end
+
   def display_posts
   	@posts.each do |post|
-  	  puts post.title
+  	  puts post.id
   	end
   end
 end
@@ -53,4 +62,5 @@ end
 @posts = PostList.new
 @posts.add_post("First Post", "Enoch Ko", "This is the body of first post")
 @posts.add_post("Second Post", "Jaemin Ko", "This is the body of second post")
+@posts.remove_post_by_id(1)
 @posts.display_posts
