3d6388a70b88b48e34e3c196d6090360f43ac6e2
enoch232 <enoch232@gmail.com>
2017-01-07 01:18:57 -0700

    make the undoing fully functional

--- commands_pattern/command_pattern_undo.rb
+++ commands_pattern/command_pattern_undo.rb
@@ -10,6 +10,29 @@ class Post
 
 end
 
+class PostManager
+  def initialize
+    @posts = []
+  end
+
+  def add_post(post)
+  	@posts << post
+  end
+
+  def remove_post_by_title(title)
+  	@posts.delete_if { |post| post.title == title}
+  end
+
+  def each(&block)
+  	index = 0
+  	while index < @posts.size
+  	  block.call(@posts[index])
+  	  index += 1
+  	end
+  end
+
+end
+
 class Command
   attr_accessor :description
 
@@ -50,22 +73,35 @@ class CompositeCommand < Command
 end
 
 class AddPostCommand < Command
-  def initialize(title, author, body)
+  attr_accessor :post
+  def initialize(title, author, body, post_store)
   	super("add a new post with the title: #{title}, author: #{author}, body: #{body}")
+  	@post = Post.new(title, author, body)
+  	@post_store = post_store
   end
 
   def execute
   	puts "adding a new post."
+  	@post_store.add_post(@post)
   end
 
   def unexecute
   	puts "removing a new post."
+  	@post_store.remove_post_by_title(@post.title)
   end
 
 end
 
 com = CompositeCommand.new
-com.add_command(AddPostCommand.new("First Post", "Enoch Ko", "This is a first post"))
-com.add_command(AddPostCommand.new("Second Post", "Jaemin Ko", "This is a second post"))
+store = PostManager.new
+com.add_command(AddPostCommand.new("First Post", "Enoch Ko", "This is a first post", store))
+com.add_command(AddPostCommand.new("Second Post", "Jaemin Ko", "This is a second post", store))
 com.execute
-com.unexecute
+# com.unexecute
+store.each do |post|
+  puts post.title
+end
+
+
+
+
